a948339bda873fcc1fe6caaad5ea05afece177dd
jdbarona <baronat.jd@gmail.com>
2025-04-21 18:54:11 -0500

    components 2

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,8 @@
+export function Card({ children }) {
+    return <div className="border shadow rounded">{children}</div>;
+  }
+  
+  export function CardContent({ children, className = "" }) {
+    return <div className={`p-4 ${className}`}>{children}</div>;
+  }
+  
---
+++ src/components/ui/input.tsx
@@ -0,0 +1,4 @@
+export function Input(props) {
+    return <input className="border rounded p-2 w-full" {...props} />;
+  }
+  
